466e43ec52c511bcf9ae7d971d0f35b03c6a9b29
xiaoyaoyao <1209709682@qq.com>
2018-03-28 14:34:10 +0800

    更新信息

---
+++ packages/react-app/webpack/webpack.base.config.js
@@ -0,0 +1,62 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+    entry:{
+        main: path.join(__dirname,"../src/index.js"),
+        common:['react','react-dom']
+    },
+    output:{
+        path:path.join(__dirname,"../build"),
+        filename:"[name].js",
+
+    },
+    resolve:{
+        extensions:['.js','.jsx','json','.css'],
+    },
+    performance: {
+        hints: false
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)?$/,
+                exclude:/(node_modules)/,
+                loader:'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use:[MiniCssExtractPlugin.loader,'css-loader'],
+
+            },
+            {
+                test:/\.html$/,
+                use:[
+                    {
+                        loader:'html-loader',
+                        options: {minimize: true}
+                    }
+                ]
+            },
+            {
+                test: /\.(ico)$/,
+                use:"raw-loader",
+            },
+            {
+                test:/\.(svg|png)$/,
+                use:'file-loader',
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: path.join(__dirname, "../public/index.html")
+        }),
+       new MiniCssExtractPlugin({
+           filename: "[name].css",
+           chunkFilename: "[id].css"
+       })
+    ]
+}
